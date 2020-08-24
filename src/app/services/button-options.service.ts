9a7ef3c8c4c9c460e29a491d5e174445e1969bd0
Cam Dziurgot <camerondziurgot@gmail.com>
2020-08-24 01:36:20 -0400

    add dummy data

--- src/app/services/button-options.service.ts
+++ src/app/services/button-options.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { RadioDisclaimerOptionModel } from '../models/radio-disclaimer-option.model';
+import * as _data from '../../assets/optionsPayloads.json';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,6 @@ export class ButtonOptionsService {
   constructor() {}
 
   public getButtonOptionsList(): Observable<Array<RadioDisclaimerOptionModel>> {
-    return new BehaviorSubject<Array<RadioDisclaimerOptionModel>>([]);
+    return new BehaviorSubject<Array<RadioDisclaimerOptionModel>>(_data.payloads.basicData);
   }
 }
