f6427050be0c8807b2d71e51b230aaeb8919ed1c
Cam Dziurgot <camerondziurgot@gmail.com>
2020-08-27 23:15:49 -0400

    update spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, TestBed, TestModuleMetadata } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, TestModuleMetadata } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
@@ -7,6 +7,8 @@ import { RadioDisclaimerButtonComponent } from './components/radio-disclaimer-bu
 import { RadioDisclaimerOptionComponent } from './components/radio-disclaimer-option/radio-disclaimer-option.component';
 
 describe('AppComponent', (): void => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
   const testConfig: TestModuleMetadata = {
     declarations: [
       AppComponent,
@@ -26,22 +28,25 @@ describe('AppComponent', (): void => {
   }));
 
   it('should create the app', (): void => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'md-radio-button-disclaimers'`, (): void => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+
     expect(app.title).toEqual('md-radio-button-disclaimers');
   });
 
   it('should render title', (): void => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    const expectedConatining = 'md-radio-button-disclaimers';
-    expect(compiled.querySelector('.content span').textContent).toContain(expectedConatining);
+
+    const element: string = fixture.nativeElement.querySelector('.content span').textContent;
+    const expectedConatining: string = 'md-radio-button-disclaimers';
+    expect(element).toContain(expectedConatining);
   });
 });
